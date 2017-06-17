c780d0d503159d96922f33cd9cfc0039ad395cc0
y2k <itwisterlx@gmail.com>
2017-06-17 16:53:13 +0300

    format code

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,40 @@
-import * as vscode from 'vscode';
+import * as vs from 'vscode';
+import { Range, Position } from 'vscode';
+import * as tmp from 'tmp';
+import * as fs from 'fs';
 
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.fntms.format', () => {
-        let editor = vscode.window.activeTextEditor; if (!editor) return
-        let text = editor.document.getText()
+const spawn = require('cross-spawn');
 
-        vscode.window.showInformationMessage('Code Formated! ' + text);
+export function activate(context: vs.ExtensionContext) {
+    let disposable = vs.commands.registerCommand('extension.fntms.format', () => {
+        const editor = vs.window.activeTextEditor; if (!editor) return
+        const text = editor.document.getText()
+
+        vs.window.withProgress({
+            title: "Format file...",
+            location: vs.ProgressLocation.Window
+        }, (x) => {
+            return new Promise(resolver => {
+                tmp.file({ postfix: ".fs" }, (_, path) => {
+                    fs.writeFile(path, text, () => {
+                        const cmd = spawn("mono", [__dirname + "/../../fantomas/Fantomas.exe", path])
+                        cmd.stdout.on('data', (data: any) => console.log(`stdout: ${data}`))
+                        cmd.stderr.on('data', (data: any) => console.log(`stderr: ${data}`))
+                        cmd.on('close', (code: number) => {
+                            fs.readFile(path, "UTF-8", (_, formated) => {
+                                editor.edit((edit) => {
+                                    edit.replace(new Range(
+                                        new Position(0, 0),
+                                        new Position(editor.document.lineCount, 0))
+                                        , formated)
+                                    resolver(undefined)
+                                })
+                            })
+                        });
+                    })
+                })
+            })
+        })
     });
     context.subscriptions.push(disposable);
-}
-
-export function deactivate() {
 }
