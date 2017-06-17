1d72ef4906715af4b548244022d266d1ae510066
y2k <itwisterlx@gmail.com>
2017-06-17 14:53:01 +0300

    init commit

---
+++ src/extension.ts
@@ -0,0 +1,14 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.fntms.format', () => {
+        let editor = vscode.window.activeTextEditor; if (!editor) return
+        let text = editor.document.getText()
+
+        vscode.window.showInformationMessage('Code Formated! ' + text);
+    });
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
