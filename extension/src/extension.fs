c524e97e31aba0314a7a6d0191b52792091f144c
y2k <itwisterlx@gmail.com>
2017-08-05 00:42:12 +0300

    refactoring

--- extension/src/extension.fs
+++ extension/src/extension.fs
@@ -8,22 +8,25 @@ open Fable.PowerPack
 module G = Fable.Import.Node.Globals
 module I = Infrastructure
 
-let private executeAsync text =
-    promise {
-        try 
-            let! path = I.createTemp { postfix = ".fs" }
-            do! I.write path text
-            do! I.shell "mono" [ G.__dirname + "/../../fantomas/Fantomas.exe"; path ]
-            let! formated = I.read path
-            do! I.replaceText formated
-        with
-        | e -> e.Message |> window.showErrorMessage |> ignore
-    }
+let private execute text = promise {
+    let! path = I.createTemp { postfix = ".fs" }
+    do! I.write path text
+    do! I.shell "mono" [ G.__dirname + "/../../fantomas/Fantomas.exe"; path ]
+    return! I.read path
+}
+
+let private executeSafe text = promise {
+    try 
+        let! formated = execute text
+        do! I.replaceText formated
+    with
+    | e -> e.Message |> window.showErrorMessage |> ignore
+}
 
 let private formatCommand () = 
     window.activeTextEditor 
     |> Option.map (fun x -> x.document.getText())
-    |> Option.map (executeAsync >> I.withProgress "Format file...")
+    |> Option.map (execute >> I.withProgress "Format file...")
 
 let activate(context : ExtensionContext) =
     commands.registerCommand("extension.fntms.format", formatCommand |> unbox<Func<_,_>>)
